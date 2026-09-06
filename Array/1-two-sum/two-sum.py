5dc523c043d02408807962579bebe2490ee9dc90
Aniket Rai <2024.aniketr@isu.ac.in>
2026-09-06 11:18:00 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (41.66%) - LeetSync

---
+++ Array/1-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    #in aaray folder check
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
